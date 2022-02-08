65cb65a018371e04c003b22733f75ff7cf302d65
Thursday the German <95447983+ThursdayTG@users.noreply.github.com>
2022-02-08 09:30:31 +0000

    Add files via upload

--- prime_numbers-main.cpp
+++ prime_numbers-main.cpp
@@ -6,85 +6,77 @@
 #include "prime_numbers-header.hpp"
 
 // using directives
-using	std::cout;
-using	std::cin;
+using std::cout;
+using std::cin;
 
 
 
 // main function
 int main()
 {
-	// used to determine whether do-while loop should be repeated manually
-	bool	restartOperator;
+	bool	restartOperator;	// used to determine whether do-while loop should be repeated manually
 
 	do
 	{
 		cout	<< "\n";
 
 		// local variable declaration - user input
-		bool	errorMessage = false;
-			// determines whether error message clean-ups need to be done after user inputs
+		bool	errorMessage = false;	// determines whether error message clean-ups need to be done after user inputs
 
 
 		// user input - lower bound
-		float	lowerBound;
-			// needs to be float for later calculations
+		float	lowerBound;		// needs to be float for later calculations
 
 		do
 		{
-			lowerBound = 1;
-				// setting default value to prevent getting stuck inside loop on unexpected input
+			lowerBound = 1;		// setting default value to prevent getting stuck inside loop on unexpected input
 
 			cout	<< " lower bound (int): ";
 			cin		>> lowerBound;
 
 			if (lowerBound < 0)
-			{	// error message
+			{
 				cout	<< " lower bound value must be an integer of at least 0! \n\n";
 				errorMessage = true;
-			}
-
+			}	// error message
 		}
 		while (lowerBound < 0);
 
 		if	(errorMessage == true)
-		{	// clean-up of error messages
+		{
 			errorMessage = false;
 			cls();
 			cout	<< " \n"
 					<< " lower bound (int): " << lowerBound << "\n";
-		}
+		}	// clean-up of error messages
 
 
 		// user input - upper bound
-		float	upperBound;
-			// needs to be float for later calculations
+		float	upperBound;		// needs to be float for later calculations
 
 		do
 		{
-			upperBound = 3.40282e+038;
-				// default value tries to prevent infinite loop repetition on unexpected error
+			upperBound = 3.40282e+038;	// default value tries to prevent infinite loop repetition on unexpected error
 
 			cout	<< " upper bound (int): ";
 			cin		>> upperBound;
 
 			if	(upperBound <= lowerBound)
-			{	// error message
+			{
 				cout	<< " upper bound value must be an integer greater than lower bound value! \n\n";
 				errorMessage = true;
-			}
-
+			}	// error message
 		}
 		while (upperBound <= lowerBound);
 
 		if (errorMessage == true)
-		{	// clean-up of error messages
+		{
 			errorMessage = false;
 			cls();
 			cout	<< " \n"
 					<< " lower bound (int): " << lowerBound << "\n"
 					<< " upper bound (int): " << upperBound << "\n";
-		}
+		}	// clean-up of error messages
 
 
 		// user input - amount columns
@@ -92,8 +84,7 @@ int main()
 
 		do
 		{
-			amountColumns = 1;
-				// setting default value to prevent getting stuck inside loop on unexpected input
+			amountColumns = 1;		// setting default value to prevent getting stuck inside loop on unexpected input
 
 			/* recommended values
 			 *
@@ -124,15 +115,15 @@ int main()
 			cout	<< "| difference ]++";
 		}
 
-		amountSeparators = amountColumns * 40;
-		if (upperBound < 1e+006)
+		amountSeparators = amountColumns * 32;
+		if (upperBound >= 1e+006)
 		{
-			amountSeparators *= 0.8;
+			amountSeparators *= 1.25;
 		}
 		amountSeparators += 4;
 
 		cout	<< "++++ \n ";
-		for ( int sep = 0; sep < amountSeparators; sep++)
+		for (int sep = 0; sep < amountSeparators; sep++)
 		{
 			cout	<< "-";
 		}
@@ -148,8 +139,7 @@ int main()
 		int		memory	= lowerBound;
 		int		newLine	= 1;
 
-		float	amountNumbers = 0;
-			// used to count amount of prime numbers
+		float	amountPrimes = 0;
 
 
 		// primary function - finding prime numbers in given range
@@ -176,8 +166,7 @@ int main()
 				if	(	dividend	<	1e+006
 					&&	upperBound	>=	1e+006)
 				{
-					cout	<< "\t";
-						// makes tables that end at 7 or more digits more coherent
+					cout	<< "\t";	// makes tables that end at 7 or more digits more coherent
 				}
 
 				cout	<< "\t | "
@@ -196,7 +185,7 @@ int main()
 					cout	<< "++++ \n ";
 				}
 
-				amountNumbers++;
+				amountPrimes++;
 			}
 
 			amountDivisions = 0;
@@ -211,6 +200,7 @@ int main()
 
 		// output - footer
 		cout	<< " \n\n ";
+
 		for (int sep = 0; sep < amountSeparators; sep++)
 		{
 			cout	<< "-";
@@ -220,12 +210,10 @@ int main()
 		for (int col = 0; col < amountColumns; col++)
 		{
 			cout	<< "++++++[ number  ";
-
 			if (upperBound >= 1e+006)
 			{
 				cout	<< "\t ";
 			}
-
 			cout	<< "| difference ]++";
 		}
 
@@ -238,11 +226,11 @@ int main()
 
 				<< " \n absolute amount of prime numbers in given range:"
 				<< " \t "
-				<< amountNumbers
+				<< amountPrimes
 
 				<< " \n percentage of prime numbers in given range:"
 				<< " \t\t "
-				<< amountNumbers / (upperBound - lowerBound + 1) * 100 << "%"
+				<< amountPrimes / (upperBound - lowerBound + 1) * 100 << "%"
 				<< " \n\n ";
 
 		for (int sep = 0; sep < amountSeparators / 2; sep++)
